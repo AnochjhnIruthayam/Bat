0dabaa23b60674d1c851e95aeac52426756ac085
sdu-rsd-group1 <aniru10@student.sdu.dk>
2014-10-24 18:22:05 +0200

    Event Finder Updated
    - Event finder updated where first point of the event is saved.
    - Problem: Cannot yet find end of event

--- BatMain.py
+++ BatMain.py
@@ -14,6 +14,11 @@ def getSampleList(path):
             sampleList.append(file)
     return sampleList
 
+#Scan the spectrogram with sliding window.
+#def scanHorizontal(img)
+#    getHeight, getWidth  = img.shape
+
+
 def findEvent(soundFilePath):
     threshold = 5
     img = cv2.imread(soundFilePath,0)
@@ -22,21 +27,32 @@ def findEvent(soundFilePath):
     #cv2.imshow('image', img)
     print img.shape
     ColumnCount = 0
+    reset = 1
+    start = 0
+    end = 0
     getHeight, getWidth  = img.shape
     getHeight -= 3 #to remove noise
     for x in range(0,getWidth):
         whiteFlag = 0
         if ColumnCount > 20:
-            cv2.circle(imgColor, (x,y), 1, (0,0,255), -1)
-            print str(img.item(y,x)) + " at position: " + str(y) + " " + str(x)
+            cv2.line(imgColor,(start,500), (end,500), (0,0,255),5)
+            #cv2.circle(imgColor, (start,500), 10, (0,0,255), -1)
+            #cv2.circle(imgColor, (end,500), 10, (0,0,255), -1)
+            #print str(img.item(y,x)) + " at position: " + str(y) + " " + str(x)
 
         for y in range(0, getHeight):
             if img.item(y,x) > threshold and whiteFlag == 0:
                 whiteFlag = 1
                 ColumnCount += 1
+                if reset == 1:
+                    start = x
+                    reset = 0
+
                 break
-            elif y == getHeight and whiteFlag == 0:
+            elif y == getHeight-1 and whiteFlag == 0:
+                reset = 1
                 ColumnCount = 0
+                end = x-1
 
     cv2.imshow('image',imgColor)
     cv2.waitKey(0)
